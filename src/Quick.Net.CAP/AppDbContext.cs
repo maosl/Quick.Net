93ce0dde61ad9e66b0d50721fbd570dcb3145ce3
毛思良 <maosiliang@35one.com>
2024-06-05 18:28:43 +0800

    CAP分布式框架支持

---
+++ src/Quick.Net.CAP/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Quick.Net.CAP.RabbitMQ.MySql
+{
+
+
+    public class AppDbContext : DbContext
+    {
+        public const string ConnectionString = "charset=utf8;server=127.0.0.1;port=3306;uid=saas_dev;pwd=9aHmc7je@gDd#IAR;Database=zm_dev03_saas_db;sslmode=none;";
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));
+        }
+    }
+}
