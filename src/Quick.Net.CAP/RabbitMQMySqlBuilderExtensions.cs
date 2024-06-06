b24b39540c84d2cddf5ff8471c593d4aa3dd7d40
毛思良 <maosiliang@35one.com>
2024-06-06 13:59:17 +0800

    引入CAP框架支持分布式事务等操作

--- src/Quick.Net.CAP/RabbitMQMySqlBuilderExtensions.cs
+++ src/Quick.Net.CAP/RabbitMQMySqlBuilderExtensions.cs
@@ -1,27 +1,29 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Quick.Net.CAP.RabbitMQ.MySql;
 using System.Runtime.CompilerServices;
 
 namespace Quick.Net.CAP
 {
-    public class RabbitMQMySqlBuilderExtensions
+    public static class RabbitMQMySqlBuilderExtensions
     {
-        public static IServiceCollection AddRabbitMQMysql(this IServiceCollection services)
+        public static void AddRabbitMQMysql(this IServiceCollection services)
         {
+
             services.AddCap(x =>
             {
-                x.UseEntityFramework<AppDbContext>();
-                x.UseRabbitMQ("localhost");
-                x.UseDashboard();
-                //x.EnableConsumerPrefetch = true;
-                x.UseDispatchingPerGroup = true;
-                x.EnableSubscriberParallelExecute = true;
-                x.FailedThresholdCallback = failed =>
-                {
-                    var logger = failed.ServiceProvider.GetService<ILogger<Startup>>();
-                    logger.LogError($@"A message of type {failed.MessageType} failed after executing {x.FailedRetryCount} several times, 
-                        requiring manual troubleshooting. Message name: {failed.Message.GetName()}");
-                };
+                 x.UseEntityFramework<AppDbContext>();
+                 x.UseRabbitMQ("localhost");
+                 x.UseDashboard();
+                 //x.EnableConsumerPrefetch = true;
+                 x.UseDispatchingPerGroup = true;
+                 x.EnableSubscriberParallelExecute = true;
+                 x.FailedThresholdCallback = failed =>
+                 {
+                     //var logger = failed.ServiceProvider.GetService<ILogger<Startup>>();
+                     //logger.LogError($@"A message of type {failed.MessageType} failed after executing {x.FailedRetryCount} several times, 
+                     //  requiring manual troubleshooting. Message name: {failed.Message.GetName()}");
+                 };
             });
         }
     }
