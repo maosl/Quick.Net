cad7bc6ae01f4e756369ba260dfbc8c19df912ad
毛思良 <maosiliang@35one.com>
2024-06-06 17:54:34 +0800

    增加SignalR的使用示例

---
+++ samples/SignalR.Client/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace SignalR.Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            rthMessage = new TextBox();
+            txtMsg1 = new TextBox();
+            txtMsg2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // rthMessage
+            // 
+            rthMessage.Location = new Point(67, 36);
+            rthMessage.Multiline = true;
+            rthMessage.Name = "rthMessage";
+            rthMessage.Size = new Size(422, 324);
+            rthMessage.TabIndex = 0;
+            // 
+            // txtMsg1
+            // 
+            txtMsg1.Location = new Point(513, 59);
+            txtMsg1.Name = "txtMsg1";
+            txtMsg1.Size = new Size(248, 23);
+            txtMsg1.TabIndex = 1;
+            // 
+            // txtMsg2
+            // 
+            txtMsg2.Location = new Point(513, 118);
+            txtMsg2.Name = "txtMsg2";
+            txtMsg2.Size = new Size(248, 23);
+            txtMsg2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(519, 187);
+            button1.Name = "button1";
+            button1.Size = new Size(242, 23);
+            button1.TabIndex = 3;
+            button1.Text = "发送消息";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(txtMsg2);
+            Controls.Add(txtMsg1);
+            Controls.Add(rthMessage);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox rthMessage;
+        private TextBox txtMsg1;
+        private TextBox txtMsg2;
+        private Button button1;
+    }
+}
---
+++ samples/SignalR.Client/Form1.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Drawing;
+
+namespace SignalR.Client
+{
+    public partial class Form1 : Form
+    {
+        private HubConnection _conn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //建立SignalR连接
+            string url = "http://localhost:5261/ChatHub";
+            _conn = new HubConnectionBuilder().WithUrl(url).Build();
+            _conn.StartAsync();
+            //SignalR客户端定义ReceiveMessage方法，中心调用客户端方法
+            _conn.On<string, string>("ReceiveMessage", RecvMsg);//接收错误日志
+
+            //断开重连方法
+            _conn.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await _conn.StartAsync();
+            };
+        }
+        private void RecvMsg(string identifiy, string errMessage)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                rthMessage.Text += $"{identifiy}:{errMessage}" + Environment.NewLine;
+            }));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //触发SignalR客户端的发送方法，客户端调用中心方法，此处的方法名称"SendMessage"应和服务器上的方法名称保持一致
+            _conn.InvokeAsync("SendMessage", this.txtMsg1.Text, this.txtMsg2.Text);
+        }
+    }
+}
