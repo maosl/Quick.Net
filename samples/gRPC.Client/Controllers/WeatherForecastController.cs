9a470198f159413b581d66fe1f88dad03467ba58
毛思良 <maosiliang@35one.com>
2024-06-07 13:53:49 +0800

    增加gRPC使用示例

---
+++ samples/gRPC.Client/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gRPC.Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5181");
+            var requestClient = new Greeter.GreeterClient(channel);
+            HelloRequest request = new HelloRequest();
+            request.Name = "maosil" ;
+            var response = requestClient.SayHello(request);
+
+            Console.WriteLine(response);
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
--- src/Quick.Net.CAP/RabbitMQMySqlBuilderExtensions.cs
+++ src/Quick.Net.CAP/RabbitMQMySqlBuilderExtensions.cs
@@ -18,12 +18,12 @@ namespace Quick.Net.CAP
                  //x.EnableConsumerPrefetch = true;
                  x.UseDispatchingPerGroup = true;
                  x.EnableSubscriberParallelExecute = true;
-                 x.FailedThresholdCallback = failed =>
-                 {
-                     //var logger = failed.ServiceProvider.GetService<ILogger<Startup>>();
-                     //logger.LogError($@"A message of type {failed.MessageType} failed after executing {x.FailedRetryCount} several times, 
-                     //  requiring manual troubleshooting. Message name: {failed.Message.GetName()}");
-                 };
+                 //x.FailedThresholdCallback = failed =>
+                 //{
+                 //    //var logger = failed.ServiceProvider.GetService<ILogger<Startup>>();
+                 //    //logger.LogError($@"A message of type {failed.MessageType} failed after executing {x.FailedRetryCount} several times, 
+                 //    //  requiring manual troubleshooting. Message name: {failed.Message.GetName()}");
+                 //};
             });
         }
     }
