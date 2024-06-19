26e836b65a24755276809a70cd82490ed676e631
毛思良 <maosiliang@35one.com>
2024-06-19 15:19:03 +0800

    Nacos 配置修改

---
+++ src/Quick.Net.Api/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Nacos.AspNetCore.V2;
+
+
+namespace Quick.Net.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddNacosAspNet(Configuration, "nacos");
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+         
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            
+        }
+    }
+}
