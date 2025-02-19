f1033dda0480b66ac4383c99d55b3474ac672858
maosil <siliang.mao@sfygroup.com>
2025-02-19 11:54:10 +0800

    新增ProductApi

---
+++ src/Quick.Net.ProductApi/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Nacos.AspNetCore.V2;
+
+namespace Quick.Net.ProductApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //配置中心
+            builder.Configuration.AddNacosV2Configuration(builder.Configuration.GetSection("NacosConfig"));
+            //服务注册
+            builder.Services.AddNacosAspNet(builder.Configuration);
+            builder.Services.AddControllers();
+    
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("Quick.Net.ProductApi", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Quick.Net.ProductApi", Version = "v1" });
+            });
+
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(option =>
+                {
+                    option.SwaggerEndpoint("/swagger/Quick.Net.ProductApi/swagger.json", "Quick.Net.ProductApi v1");
+                });
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
